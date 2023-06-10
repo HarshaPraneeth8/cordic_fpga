223ef74bbafba2016c670b2f0bd50e9f1b15311c
hp <72025415+HarshaPraneeth8@users.noreply.github.com>
2023-06-10 15:20:41 +0530

    Update clk_div.v

--- Design_files/clk_div.v
+++ Design_files/clk_div.v
@@ -4,11 +4,9 @@ input clock_in;
 output reg clock_out; 
 reg[27:0] counter=28'd0;
 parameter DIVISOR = 28'd470;
-// The frequency of the output clk_out
-//  = The frequency of the input clk_in divided by DIVISOR
-// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
-// You will modify the DIVISOR parameter value to 28'd50.000.000
-// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+// The frequency of the output clk_out = The frequency of the input clk_in divided by DIVISOR
+// For example, Fclk_in = 100Mhz, if you want to get 212.77KHz signal to blink LEDs
+// The DIVISOR parameter value will be 28'd470
 always @(posedge clock_in)
 begin
  counter <= counter + 28'd1;
